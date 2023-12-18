f73711df87aed44cce2d3a453fa05488ceb79896
Brian Fitzgerald <brian@stability.ai>
2023-12-17 22:04:25 -0600

    patching

--- __init__.py
+++ __init__.py
@@ -1,22 +1,26 @@
+from typing import List
 import torch
 from comfy.model_patcher import ModelPatcher
 import comfy.samplers
 from nodes import common_ksampler
+import random
+import torch
+import comfy.ldm.modules.attention
+from comfy.ldm.modules.attention import (
+    attention_basic,
+    optimized_attention,
+)
+
+import comfy.ops
+
+ops = comfy.ops.disable_weight_init
 
 T = torch.Tensor
 
 
-class WindowedAttentionProcessor:
-    def __init__(self, t_start: int, t_end: int):
-        self.t_start = t_start
-        self.t_end = t_end
-
-    def __call__(self, q, k, v, extra_options):
-        print("attn with", self.t_start, self.t_end)
-        return q, k, v
-
-
-def get_attn_windows(video_length: int, window_size: int = 16, stride: int = 4):
+def get_attn_windows(
+    video_length: int, window_size: int = 16, stride: int = 4
+) -> List[tuple[int, int]]:
     num_blocks_time = (video_length - window_size) // stride + 1
     views = []
     for i in range(num_blocks_time):
@@ -26,7 +30,83 @@ def get_attn_windows(video_length: int, window_size: int = 16, stride: int = 4):
     return views
 
 
-class KSamplerFreeNoise:
+class WindowState:
+    t_start: int
+    t_end: int
+
+    _instance = None
+
+    @classmethod
+    def instance(cls):
+        if cls._instance is None:
+            cls._instance = cls()
+        return cls._instance
+
+def attn_windowed(q, k, v, extra_options):
+    state = WindowState.instance()
+    print(f"forward {state.t_start} {state.t_end} {extra_options}")
+    heads = extra_options["n_heads"]
+    cond_or_uncond = extra_options["cond_or_uncond"]
+    breakpoint()
+    q = q[state.t_start:state.t_end]
+    k = k[state.t_start:state.t_end]
+    v = v[state.t_start:state.t_end]
+    b = q.shape[0] // len(cond_or_uncond)
+    return optimized_attention(q, k, v, heads=heads)
+
+def set_model_patch_replace(model, patch_kwargs, key):
+    print(f"patching {key} with kwargs {patch_kwargs}")
+    to = model.model_options["transformer_options"]
+    if "patches_replace" not in to:
+        to["patches_replace"] = {}
+    if "attn2" not in to["patches_replace"]:
+        to["patches_replace"]["attn2"] = {}
+    if key not in to["patches_replace"]["attn2"]:
+        to["patches_replace"]["attn2"][key] = attn_windowed
+    else:
+        print(f"already patched {key}")
+
+
+def patch_model(model: ModelPatcher, is_sdxl: bool = False):
+    patch_kwargs = {"number": 0}
+
+    if not is_sdxl:
+        for id in [1, 2, 4, 5, 7, 8]:  # id of input_blocks that have cross attention
+            set_model_patch_replace(model, patch_kwargs, ("input", id))
+            patch_kwargs["number"] += 1
+        for id in [
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+        ]:  # id of output_blocks that have cross attention
+            set_model_patch_replace(model, patch_kwargs, ("output", id))
+            patch_kwargs["number"] += 1
+        set_model_patch_replace(model, patch_kwargs, ("middle", 0))
+    else:
+        for id in [4, 5, 7, 8]:  # id of input_blocks that have cross attention
+            block_indices = range(2) if id in [4, 5] else range(10)  # transformer_depth
+            for index in block_indices:
+                set_model_patch_replace(model, patch_kwargs, ("input", id, index))
+                patch_kwargs["number"] += 1
+        for id in range(6):  # id of output_blocks that have cross attention
+            block_indices = (
+                range(2) if id in [3, 4, 5] else range(10)
+            )  # transformer_depth
+            for index in block_indices:
+                set_model_patch_replace(model, patch_kwargs, ("output", id, index))
+                patch_kwargs["number"] += 1
+        for index in range(10):
+            set_model_patch_replace(model, patch_kwargs, ("middle", 0, index))
+            patch_kwargs["number"] += 1
+
+
+class KSamplerExtended:
     @classmethod
     def INPUT_TYPES(cls):
         return {
@@ -84,18 +164,24 @@ class KSamplerFreeNoise:
         window_stride: int,
         denoise=1.0,
     ):
+        random.seed(seed)
         latent_tensor = latent_image["samples"]
         video_num_frames = latent_tensor.shape[0]
+
         attn_windows = get_attn_windows(video_num_frames, window_size, window_stride)
+
         m = model.clone()
         latents = latent_tensor
         out_latents = torch.zeros_like(latents)
-        out_dict = {}
+        out_dict = {"samples": out_latents}
         print(f"computing {len(attn_windows)} windows")
+        patch_model(m)
         for i in range(len(attn_windows)):
             t_start, t_end = attn_windows[i]
             print(f"process window {i}/{len(attn_windows)} ({t_start}, {t_end})")
-            m.set_model_attn1_patch(WindowedAttentionProcessor(t_start, t_end))
+            WindowState.instance().t_start = t_start
+            WindowState.instance().t_end = t_end
+
             latents = common_ksampler(
                 m,
                 seed,
@@ -107,15 +193,13 @@ class KSamplerFreeNoise:
                 negative,
                 latent_image,
                 denoise=denoise,
-            )
-            breakpoint()
-            out_latents[t_start:t_end] = latents[t_start:t_end]
+            )[0]
+            out_latents[t_start:t_end] = latents["samples"][t_start:t_end]
             out_dict["samples"] = out_latents
-            return (out_dict)
 
-        return (out_dict)
+        return (out_dict,)
 
 
 NODE_CLASS_MAPPINGS = {
-    "KSamplerFreeNoise": KSamplerFreeNoise,
+    "KSamplerExtended": KSamplerExtended,
 }
